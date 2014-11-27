8d6427bdf7b53daa522be13a7528776f8bb1e8a5
Frazer Seymour <pseymour@uoguelph.ca>
2014-11-27 16:22:52 -0500

    Missing string.

--- AudioGraph.js
+++ AudioGraph.js
@@ -110,9 +110,11 @@ function decodeArrayBuffer(input) {
 }
 
 function decode(input, arrayBuffer) {
+	var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
     //get last chars to see if are valid
-    var lkey1 = this._keyStr.indexOf(input.charAt(input.length-1));		 
-    var lkey2 = this._keyStr.indexOf(input.charAt(input.length-2));		 
+    var lkey1 = _keyStr.indexOf(input.charAt(input.length-1));		 
+    var lkey2 = _keyStr.indexOf(input.charAt(input.length-2));		 
 
     var bytes = (input.length/4) * 3;
     if (lkey1 == 64) bytes--; //padding chars, so skip
@@ -133,10 +135,10 @@ function decode(input, arrayBuffer) {
     
     for (i=0; i<bytes; i+=3) {	
         //get the 3 octects in 4 ascii chars
-        enc1 = this._keyStr.indexOf(input.charAt(j++));
-        enc2 = this._keyStr.indexOf(input.charAt(j++));
-        enc3 = this._keyStr.indexOf(input.charAt(j++));
-        enc4 = this._keyStr.indexOf(input.charAt(j++));
+        enc1 = _keyStr.indexOf(input.charAt(j++));
+        enc2 = _keyStr.indexOf(input.charAt(j++));
+        enc3 = _keyStr.indexOf(input.charAt(j++));
+        enc4 = _keyStr.indexOf(input.charAt(j++));
 
         chr1 = (enc1 << 2) | (enc2 >> 4);
         chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
