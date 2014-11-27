5ad3526abd87149e4e943fdef3cade4a4722545d
Frazer Seymour <pseymour@uoguelph.ca>
2014-11-27 16:20:26 -0500

    Stole some code, trying a different attempt at decoding base64 to bufferarray.

--- AudioGraph.js
+++ AudioGraph.js
@@ -85,7 +85,7 @@ function loadDing() {
             function(response) {
                 console.log(response);
                 context.decodeAudioData(
-                    str2ab(response.content),
+                    decodeArrayBuffer(response.content),
                     function(buffer) {
                         ding = context.createBufferSource();
                         ding.buffer = buffer;
@@ -100,13 +100,54 @@ function loadDing() {
     });
 }
 
-function str2ab(str) {
-    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-    var bufView = new Uint16Array(buf);
-    for (var i=0, strLen=str.length; i<strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
+/* will return a  Uint8Array type */
+function decodeArrayBuffer(input) {
+    var bytes = (input.length/4) * 3;
+    var ab = new ArrayBuffer(bytes);
+    decode(input, ab);
+    
+    return ab;
+},
+
+function decode(input, arrayBuffer) {
+    //get last chars to see if are valid
+    var lkey1 = this._keyStr.indexOf(input.charAt(input.length-1));		 
+    var lkey2 = this._keyStr.indexOf(input.charAt(input.length-2));		 
+
+    var bytes = (input.length/4) * 3;
+    if (lkey1 == 64) bytes--; //padding chars, so skip
+    if (lkey2 == 64) bytes--; //padding chars, so skip
+    
+    var uarray;
+    var chr1, chr2, chr3;
+    var enc1, enc2, enc3, enc4;
+    var i = 0;
+    var j = 0;
+    
+    if (arrayBuffer)
+        uarray = new Uint8Array(arrayBuffer);
+    else
+        uarray = new Uint8Array(bytes);
+    
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    
+    for (i=0; i<bytes; i+=3) {	
+        //get the 3 octects in 4 ascii chars
+        enc1 = this._keyStr.indexOf(input.charAt(j++));
+        enc2 = this._keyStr.indexOf(input.charAt(j++));
+        enc3 = this._keyStr.indexOf(input.charAt(j++));
+        enc4 = this._keyStr.indexOf(input.charAt(j++));
+
+        chr1 = (enc1 << 2) | (enc2 >> 4);
+        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+        chr3 = ((enc3 & 3) << 6) | enc4;
+
+        uarray[i] = chr1;			
+        if (enc3 != 64) uarray[i+1] = chr2;
+        if (enc4 != 64) uarray[i+2] = chr3;
     }
-    return buf;
+
+    return uarray;	
 }
 
 /**
