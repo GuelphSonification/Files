02b4604ca1b2dda3bf22fffe1a4ac7a4d150c58a
Frazer Seymour <frazer@seymourandco.com>
2014-11-27 00:41:46 -0500

    Dramatically scuppered SoundFile.js and soundtest.

--- SoundFile.js
+++ SoundFile.js
@@ -1,35 +1,35 @@
-function BufferLoader(context, urlList, callback) {
-    this.context = context;
-    this.urlList = urlList;
-    this.onload = callback;
-    this.bufferList = new Array();
-    this.loadCount = 0;
-}
+window.onload = init;
+var context;
+var bufferLoader;
 
-BufferLoader.prototype.loadBuffer = function(url, index) {
+function init() {
+    // Fix up prefixing
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    context = new AudioContext();
+
     // Load buffer asynchronously
     var request = new XMLHttpRequest();
-    request.open("GET", url, true);
+    request.open("GET", 'http://guelphsonification.github.io/Files/ding.wav', true);
     request.responseType = "arraybuffer";
-
-    var loader = this;
 
     request.onload = function() {
         // Asynchronously decode the audio file data in request.response
-        loader.context.decodeAudioData(
-        request.response,
-        function(buffer) {
-            if (!buffer) {
-            alert('error decoding file data: ' + url);
-            return;
+        context.decodeAudioData(
+            request.response,
+            function(buffer) {
+                if (!buffer) {
+                    alert('error decoding file data: ' + url);
+                    return;
+                }
+                var source1 = context.createBufferSource();
+                source1.buffer = buffer;
+
+                source1.connect(context.destination);
+                source1.start(0);
+            },
+            function(error) {
+                console.error('decodeAudioData error', error);
             }
-            loader.bufferList[index] = buffer;
-            if (++loader.loadCount == loader.urlList.length)
-            loader.onload(loader.bufferList);
-        },
-        function(error) {
-            console.error('decodeAudioData error', error);
-        }
         );
     }
 
@@ -38,39 +38,4 @@ BufferLoader.prototype.loadBuffer = function(url, index) {
     }
 
     request.send();
-    }
-
-    BufferLoader.prototype.load = function() {
-    for (var i = 0; i < this.urlList.length; ++i)
-    this.loadBuffer(this.urlList[i], i);
 }
-
-
-window.onload = init;
-var context;
-var bufferLoader;
-
-function init() {
-// Fix up prefixing
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-context = new AudioContext();
-
-bufferLoader = new BufferLoader(
-    context,
-        [
-            'http://guelphsonification.github.io/Files/ding.wav',
-        ],
-        finishedLoading
-    );
-
-    bufferLoader.load();
-}
-
-function finishedLoading(bufferList) {
-    // Create two sources and play them both together.
-    var source1 = context.createBufferSource();
-    source1.buffer = bufferList[0];
-
-    source1.connect(context.destination);
-    source1.start(0);
-} 
